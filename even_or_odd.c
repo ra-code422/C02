ae49db750e4c80f46864dd02b48e64746807b5b2
SOUMAILA HAMIDOU Rafiatou <soumailahamidourafiatou@iMac-de-SOUMAILA.local>
2025-07-21 23:17:36 +0100

    rafia

---
+++ even_or_odd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+   scanf("%d", &a);
+  if(a % 2== 0) {
+    printf("%d est pair.\n",a);
+  }
+ else
+ {
+    printf("%d est impair.\n",a);
+ }
+
+return 0;   
+}
