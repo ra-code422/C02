db477aedcec444f809c2490b26b4d72c1ebb7e47
SOUMAILA HAMIDOU Rafiatou <soumailahamidourafiatou@iMac-de-SOUMAILA.local>
2025-07-23 02:50:45 +0100

    rafia

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-int a,b;
+int a , b;
 scanf("%d %d", &a, &b);
-a= a+b;
-b= a-b;
-a= a-b;
+a = a + b;
+b = a - b;
+a = a - b;
 printf("Après échange: a = %d, b = %d\n", a, b);
 
 return 0;
